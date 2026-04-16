7a30bd1f1b89a1b9f41cfbbf5ab038a95164dc69
Eka Gunawan <56123064+radengunawan@users.noreply.github.com>
2026-04-16 09:15:46 +0800

    Add: edit-distance - submission-19

---
+++ Data Structures & Algorithms/edit-distance/submission-19.py
@@ -0,0 +1,36 @@
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+
+        if not word1 and not word2:
+            return 0
+        
+        M, N = len(word1), len(word2)
+
+        if not word1:
+            return N
+        elif not word2:
+            return M
+        
+        cache = [[None]*N for _ in range(M)]
+
+        def minDis_from(i,j):
+            if i == M:
+                return (N - j)
+            if j == N:
+                return (M - i)
+            if cache[i][j] is not None:
+                return cache[i][j]
+            
+            res = 0
+            if word1[i] == word2[j]:
+                res = minDis_from(i+1,j+1)
+            else:
+                insert = minDis_from(i,j+1)
+                delete =  minDis_from(i+1,j)
+                replace =  minDis_from(i+1,j+1)
+                res = 1+ min(insert, delete, replace)
+            cache[i][j] = res
+            return res
+
+        return minDis_from(0,0)
+        
