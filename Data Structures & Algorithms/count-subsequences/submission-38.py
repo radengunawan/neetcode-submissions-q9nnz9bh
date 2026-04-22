bdba97ebad634a865fa8533435514ac44b4c640d
Eka Gunawan <56123064+radengunawan@users.noreply.github.com>
2026-04-22 16:30:45 +0800

    Add: count-subsequences - submission-38

---
+++ Data Structures & Algorithms/count-subsequences/submission-38.py
@@ -0,0 +1,30 @@
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+
+        if not s or not t:
+            return 0
+        
+        M, N = len(s), len(t)
+
+        if N > M or not set(t).issubset(set(s)):
+            return 0
+        
+        sys.setrecursionlimit(2000)
+        cache = [[None]*N for _ in range(M)]
+        
+        def numDis_from(i,j):
+            if j==N:
+                return 1
+            if i==M:
+                return 0
+
+            if cache[i][j] is not None:
+                return cache[i][j]
+            
+            res = numDis_from(i+1,j)
+            if s[i] == t[j]:
+                res += numDis_from(i+1,j+1)
+            cache[i][j] = res
+            return res
+
+        return numDis_from(0,0)
