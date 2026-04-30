dd48b20601d2bbe444a13918a4af592233961fd8
Eka Gunawan <56123064+radengunawan@users.noreply.github.com>
2026-04-30 17:43:48 +0800

    Add: coin-change - submission-19

---
+++ Data Structures & Algorithms/coin-change/submission-19.py
@@ -0,0 +1,25 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        N = amount
+        if N ==0:
+            return 0
+        
+        q = deque([N])
+        seen = [False]*(N+1)
+        seen[0] = True
+        level = 0
+
+        while q:
+            level +=1
+            for _ in range (len(q)):
+                remain_current = q.popleft()
+                for coin in coins:
+                    remain_next = remain_current - coin
+                    if remain_next == 0:
+                        return level
+                    if remain_next < 0 or seen[remain_next]:
+                        continue
+                    seen[remain_next] = True
+                    q.append(remain_next)
+        
+        return -1
