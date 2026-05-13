8d61d5bfc5021981b352dfe8070f5bc22430b1a5
Eka Gunawan <56123064+radengunawan@users.noreply.github.com>
2026-05-13 23:21:24 +0700

    Add: coin-change - submission-29

---
+++ Data Structures & Algorithms/coin-change/submission-29.py
@@ -0,0 +1,26 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        M,N = len(coins), amount
+        if N == 0:
+            return 0
+        
+        cache = [None]*(N+1)
+
+        def coinChange_from(remaining_now):
+            if remaining_now == 0:
+                return 0
+            if cache[remaining_now] is not None:
+                return cache[remaining_now]
+            
+            res = N+1
+            for coin in coins:
+                remaining_next = remaining_now - coin
+                if remaining_next >= 0:
+                    res = min(res, 1 + coinChange_from(remaining_next))
+            cache[remaining_now] = res
+            return res
+        
+        final_res = coinChange_from(N)
+
+        return -1 if final_res >= N+1 else final_res
+        
