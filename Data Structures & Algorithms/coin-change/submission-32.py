7de9e357db29bc13e225704c18358043153e3921
Eka Gunawan <56123064+radengunawan@users.noreply.github.com>
2026-05-16 01:13:19 +0700

    Add: coin-change - submission-32

---
+++ Data Structures & Algorithms/coin-change/submission-32.py
@@ -0,0 +1,26 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        N = amount
+        if N == 0:
+            return 0
+
+        queuez = deque([N])
+        seen = [False]*(N+1)
+        seen[N] = True
+        number_of_coins = 0
+
+        while queuez:
+            number_of_coins +=1
+            for _ in range(len(queuez)):
+                current_val = queuez.popleft()
+                for coin in coins:
+                    next_val = current_val - coin
+                    if next_val == 0:
+                        return number_of_coins
+                    if next_val < 0 or seen[next_val]:
+                        continue
+                    
+                    seen[next_val] = True
+                    queuez.append(next_val)
+        
+        return -1
